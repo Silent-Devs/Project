e4639d3a4c7876275b4950992216d0239fb7262c
CihaoZhang <122197828+CihaoZhang@users.noreply.github.com>
2024-04-05 18:44:33 -0400

    ToDoList

---
+++ src/main/java/logic/ToDoList.java
@@ -0,0 +1,33 @@
+package logic;
+
+import java.util.Date;
+
+public class ToDoList {
+
+    public Date date;
+
+    public String Event;
+
+    public Date EventDate;
+
+    public int priority;
+
+    public Subject subject;
+
+    public boolean checked;
+
+
+    public ToDoList(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static void addEvent(){
+
+    }
+}
