111e10519881c0b9c55640ea0985d52132303a00
Sureau2020 <wen39717164@gmail.com>
2024-04-06 00:05:23 -0400

    welcome+contentable
    welcome+contenttble

--- src/main/kotlin/routes/web/Index.kt
+++ src/main/kotlin/routes/web/Index.kt
@@ -9,10 +9,77 @@ fun Route.index() {
     get("/") {
         call.respondHtml {
             head {
-                title { +"Ktor Server" }
+                title { +"ALL-IN-ONE-TOOL-BOX" }
+                meta {charset="UTF-8" }
+                link(rel = "stylesheet", href = "/css/style.css", type = "text/css")
+                script { src = "/js/script.js"}
+                meta {name="viewport"; content="width=device-width, initial-scale=1.0"}
+
             }
             body {
-                p { +"Hello, Ktor!" }
+                div ("column circle"){
+                    div("circle-container"){
+                        div("choice-one"){
+                            a {
+                                href = "/"
+                                +"Simple "
+
+                            }
+                        }
+                        div("choice-two"){
+                            a {
+                                href = "/"
+                                +"Simple2 "
+
+                            }
+                        }
+                        div("choice-three"){
+                            a {
+                                href = "/"
+                                +"Simple3 "
+
+                            }
+                        }
+                        div("choice-four"){
+                            a {
+                                href = "/"
+                                +"Simple 4"
+
+                            }
+                        }
+                    }
+                }
+                div("column right"){
+                    div ("board"){
+                        h1 {
+                        +"ALL IN ONE TOOL"
+                    }
+                        a {onClick="load_list()"
+                            +"All tools"
+                        }
+                    }
+                }
+                div("main-interface"){
+                    div ("links"){
+                        p {
+                            +"example1"
+                        }
+                    }
+                    div ("links"){
+                        p {
+                            +"example1"
+                        }
+                    }
+                    div ("links"){
+                        p {
+                            +"example1"
+                        }
+                    }
+
+                }
+
+
+
             }
         }
     }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+
+
+
+});
+
+function load_list() {
+    document.querySelector('.circle').style.animationPlayState = 'running';
+    document.querySelector('.right').style.animationPlayState = 'running';
+    document.querySelector('.circle').addEventListener('animationend', () => {
+        document.querySelector('.circle').remove();
+        document.querySelector('.right').remove();
+        document.querySelector('.main-interface').style.animationPlayState = 'running';
+    });
+
+
+
+}
+
+function submitForm() {
+    document.querySelector('.main-interface').style.display = 'none';
+    document.querySelector('.loading-screen').style.display = 'block';
+    sleep(2000).then(() => {
+        document.querySelector('#real-submit').click();
+    });
+}
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
