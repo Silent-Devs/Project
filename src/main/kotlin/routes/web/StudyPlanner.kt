b11f8dced17130a0c0d9abf6b779f1e580f56614
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-04-05 22:12:10 -0400

    Clean Code

--- src/main/kotlin/routes/web/StudyPlanner.kt
+++ src/main/kotlin/routes/web/StudyPlanner.kt
@@ -6,10 +6,28 @@ import io.ktor.server.html.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import kotlinx.html.*
-import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
-import org.intellij.markdown.html.HtmlGenerator
-import org.intellij.markdown.parser.MarkdownParser
 import utils.ConfigUtil
+import utils.MarkdownUtil
+
+fun BODY.continueStudyPlanner() {
+    form(action = "/study-planner", method = FormMethod.post) {
+        textInput {
+            name = "description"
+            placeholder = "Tell the AI what do you want to change"
+        }
+        select {
+            name = "first"
+            style = "display: none"
+            option { +"Second" }
+        }
+        br
+        button {
+            type = ButtonType.submit
+            id = "submissionButton"
+            +"Submit"
+        }
+    }
+}
 
 fun Route.studyPlanner() {
     val apiConfig = ConfigUtil.loadConfig().api.gemini
@@ -54,38 +72,20 @@ fun Route.studyPlanner() {
                     beforePrompt = "Hi, please generate a study plan in a day for me. Here is what I want to have:",
                     afterPrompt = "Please return in pure Markdown format with bullet lists. Thanks!"
                 )
-                val flavour = CommonMarkFlavourDescriptor()
-                val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(response)
-                val html = HtmlGenerator(response, parsedTree, flavour).generateHtml()
+
                 call.respondHtml {
                     head {
                         title { +"Study Plan" }
                     }
                     body {
-                        unsafe { raw(html) }
+                        unsafe { raw(MarkdownUtil.toHtml(response)) }
                         br {}
                         p {
                             +"If you like the study plan, save it to your device or print it out."
                             +" "
                             +"If you want to change something, tell the AI want do you want to change."
                         }
-                        form(action = "/study-planner", method = FormMethod.post) {
-                            textInput {
-                                name = "description"
-                                placeholder = "Tell the AI what do you want to change"
-                            }
-                            select {
-                                name = "first"
-                                style = "display: none"
-                                option { +"Second" }
-                            }
-                            br
-                            button {
-                                type = ButtonType.submit
-                                id = "submissionButton"
-                                +"Submit"
-                            }
-                        }
+                        continueStudyPlanner()
                     }
                 }
             } else {
@@ -94,38 +94,19 @@ fun Route.studyPlanner() {
                     beforePrompt = "Hi, please change the study plan according to my request:",
                     afterPrompt = "Please return the new result in pure Markdown format with bullet lists. Thanks!"
                 )
-                val flavour = CommonMarkFlavourDescriptor()
-                val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(response)
-                val html = HtmlGenerator(response, parsedTree, flavour).generateHtml()
                 call.respondHtml {
                     head {
                         title { +"Study Plan" }
                     }
                     body {
-                        unsafe { raw(html) }
+                        unsafe { raw(MarkdownUtil.toHtml(response)) }
                         br {}
                         p {
                             +"If you are satisfied with the new study plan, save it to your device or print it out."
                             +" "
                             +"If you want to change something, you cal still tell the AI what do you want to change."
                         }
-                        form(action = "/study-planner", method = FormMethod.post) {
-                            textInput {
-                                name = "description"
-                                placeholder = "Tell the AI what do you want to change"
-                            }
-                            select {
-                                name = "first"
-                                style = "display: none"
-                                option { +"Second" }
-                            }
-                            br
-                            button {
-                                type = ButtonType.submit
-                                id = "submissionButton"
-                                +"Submit"
-                            }
-                        }
+                        continueStudyPlanner()
                     }
                 }
             }
